511fe7a09e4f5d32804e6e92ad7e14032b9df4b3
Raj <rpal778866@gmail.com>
2025-05-03 21:30:47 +0530

    chore: small fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,8 @@ const getSingleUser = async (req, res) => {
 };
 
 const getCurrentUser = async (req, res) => {
+  console.log("✅ Cookies received:", req.cookies);
+  console.log("✅ Signed cookies:", req.signedCookies);
   const user = await User.findById(req.user.userId).select("-googleId");
   if (!user) {
     throw new CustomError.NotFoundError("No user found with this ID");
