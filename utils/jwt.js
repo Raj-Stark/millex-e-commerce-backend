56f02dd57f74a86eccd8c78cc2ca3b99fd7647ac
Raj <rpal778866@gmail.com>
2025-05-03 21:24:41 +0530

    chore: small fix

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,6 +12,22 @@ const isTokenValid = ({ token }) => {
   return jwt.verify(token, process.env.JWT_SECRET);
 };
 
+// const attachCookiesToResponse = ({ res, user }) => {
+//   const token = createJWT({ payload: user });
+//   const oneDay = 1000 * 60 * 60 * 24;
+
+//   const isProduction = process.env.NODE_ENV === "production";
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     expires: new Date(Date.now() + oneDay),
+//     secure: isProduction, // only true if behind HTTPS
+//     signed: true,
+//     sameSite: isProduction ? "Strict" : "Lax", // allow cookie in dev
+//     path: "/",
+//   });
+// };
+
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
@@ -21,9 +37,9 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: isProduction, // only true if behind HTTPS
+    secure: isProduction ? true : false, // ❗ allow over HTTP during dev
     signed: true,
-    sameSite: isProduction ? "Strict" : "Lax", // allow cookie in dev
+    sameSite: isProduction ? "None" : "Lax", // ❗ 'None' required for cross-origin with HTTPS, 'Lax' works for dev
     path: "/",
   });
 };
