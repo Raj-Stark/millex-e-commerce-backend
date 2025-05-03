85fc0d70cc229e8adf4a4212d31d8b4502986fbd
Raj <rpal778866@gmail.com>
2025-05-03 21:33:44 +0530

    chore: small fix

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,22 +12,6 @@ const isTokenValid = ({ token }) => {
   return jwt.verify(token, process.env.JWT_SECRET);
 };
 
-// const attachCookiesToResponse = ({ res, user }) => {
-//   const token = createJWT({ payload: user });
-//   const oneDay = 1000 * 60 * 60 * 24;
-
-//   const isProduction = process.env.NODE_ENV === "production";
-
-//   res.cookie("token", token, {
-//     httpOnly: true,
-//     expires: new Date(Date.now() + oneDay),
-//     secure: isProduction, // only true if behind HTTPS
-//     signed: true,
-//     sameSite: isProduction ? "Strict" : "Lax", // allow cookie in dev
-//     path: "/",
-//   });
-// };
-
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
@@ -37,12 +21,28 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: isProduction ? true : false, // ❗ allow over HTTP during dev
+    secure: isProduction, // only true if behind HTTPS
     signed: true,
-    sameSite: isProduction ? "None" : "Lax", // ❗ 'None' required for cross-origin with HTTPS, 'Lax' works for dev
+    sameSite: isProduction ? "Strict" : "Lax", // allow cookie in dev
     path: "/",
   });
 };
+
+// const attachCookiesToResponse = ({ res, user }) => {
+//   const token = createJWT({ payload: user });
+//   const oneDay = 1000 * 60 * 60 * 24;
+
+//   const isProduction = process.env.NODE_ENV === "production";
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     expires: new Date(Date.now() + oneDay),
+//     secure: isProduction ? true : false, // ❗ allow over HTTP during dev
+//     signed: true,
+//     sameSite: "None",
+//     path: "/",
+//   });
+// };
 
 module.exports = {
   createJWT,
